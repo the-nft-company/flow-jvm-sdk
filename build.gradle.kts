3c1415ee5981ca140a64d09e7c6a422a78a51093
Brian Dilley <briandilley@briandilley.com>
2021-04-19 21:47:44 -0700

    snapshot version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // configuration variables
 val javaTargetVersion   = "1.8"
 val defaultGroupId      = "org.onflow"
-val defaultVersion      = "0.2.3-SNAPSHOT"
+val defaultVersion      = "0.2.4-SNAPSHOT"
 
 // other variables
 
