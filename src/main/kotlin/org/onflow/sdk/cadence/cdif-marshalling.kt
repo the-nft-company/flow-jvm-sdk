e1f8fc4ecc1727fe95628ba005770ff6880f4529
Brian Dilley <briandilley@briandilley.com>
2021-05-31 19:05:57 -0700

    some minor formatting stuff

--- src/main/kotlin/org/onflow/sdk/cadence/cdif-marshalling.kt
+++ src/main/kotlin/org/onflow/sdk/cadence/cdif-marshalling.kt
@@ -16,10 +16,10 @@ annotation class CDIFConverter(
 )
 
 interface CadenceDataInterchangeFormatConverter<T> {
-    fun unmarshall(value: Field<*>): T {
+    fun unmarshall(value: Field<*>) : T {
         throw UnsupportedOperationException("${this::class.simpleName} cannot convert from cdif")
     }
-    fun marshall(value: T): Field<*> {
+    fun marshall(value: T) : Field<*> {
         throw UnsupportedOperationException("${this::class.simpleName} cannot convert to cdif")
     }
 }
@@ -32,7 +32,7 @@ object Marshalling {
     @JvmOverloads
     @Synchronized
     @Suppress("UNCHECKED_CAST")
-    fun <T : Any> getConverter(type: KClass<out T>, cache: Boolean = true): CadenceDataInterchangeFormatConverter<T> {
+    fun <T : Any> getConverter(type: KClass<out T>, cache: Boolean = true) : CadenceDataInterchangeFormatConverter<T> {
         var ret = if (cache) {
             converterCache[type]
         } else {
@@ -54,57 +54,57 @@ object Marshalling {
     }
 
     @JvmStatic
-    fun <T: Any> unmarshall(type: KClass<T>, value: Field<*>): T = getConverter(type).unmarshall(value)
+    fun <T: Any> unmarshall(type: KClass<T>, value: Field<*>) : T = getConverter(type).unmarshall(value)
 
     @JvmStatic
     @JvmOverloads
-    fun <T: Any> marshall(value: T, clazz: KClass<out T> = value::class): Field<*> = getConverter(clazz).marshall(value)
+    fun <T: Any> marshall(value: T, clazz: KClass<out T> = value::class) : Field<*> = getConverter(clazz).marshall(value)
 }
 
-fun <T : Field<*>> cdif(block: CDIFBuilder.() -> T): T = block(CDIFBuilder())
+fun <T : Field<*>> cdif(block: CDIFBuilder.() -> T) : T = block(CDIFBuilder())
 
 class CDIFBuilder {
-    fun <T: Any> marshall(value: T, clazz: KClass<out T> = value::class): Field<*> = Flow.marshall(value, clazz)
-    fun void(): VoidField = VoidField()
-    fun <T> optional(block: CDIFBuilder.() -> Field<T>?): OptionalField<T> = OptionalField(block())
-    fun <T> optional(value: Field<T>?): OptionalField<T> = OptionalField(value)
-    fun boolean(value: Boolean): BooleanField = BooleanField(value)
-    fun string(value: String): StringField = StringField(value)
-    fun array(value: Iterable<Field<*>>): ArrayField = ArrayField(value)
-    fun <T> array(value: Iterable<T>, mapper:  CDIFBuilder.(T) -> Field<*> ): ArrayField = ArrayField(value.map { mapper(it) })
-    fun array(block: CDIFBuilder.() -> Iterable<Field<*>>): ArrayField = ArrayField(block().toList().toTypedArray())
-    fun dictionary(value: Iterable<DictionaryFieldEntry>): DictionaryField = DictionaryField(value)
-    fun dictionary(block: CDIFBuilder.() -> Array<DictionaryFieldEntry>): DictionaryField = DictionaryField(block().toList().toTypedArray())
-    fun dictionaryOfMap(block: CDIFBuilder.() -> Map<Field<*>, Field<*>>): DictionaryField = DictionaryField(block().map { DictionaryFieldEntry(it.key, it.value) })
-    fun dictionaryOfNamedMap(block: CDIFBuilder.() -> Map<String, Field<*>>): DictionaryField = DictionaryField(block().map { DictionaryFieldEntry(StringField(it.key), it.value) })
-    fun dictionaryOfPairs(block: CDIFBuilder.() -> Iterable<Pair<Field<*>, Field<*>>>): DictionaryField = DictionaryField(block().map { DictionaryFieldEntry(it.first, it.second) })
-    fun dictionaryOfNamedPairs(block: CDIFBuilder.() -> Iterable<Pair<String, Field<*>>>): DictionaryField = DictionaryField(block().map { DictionaryFieldEntry(StringField(it.first), it.second) })
-    fun address(value: String): AddressField = AddressField(value)
-    fun address(value: ByteArray): AddressField = AddressField(value)
-    fun path(value: PathValue): PathField = PathField(value)
-    fun path(domain: String, identifier: String): PathField = PathField(PathValue(domain, identifier))
-    fun capability(value: CapabilityValue): CapabilityField = CapabilityField(value)
-    fun capability(path: String, address: String, borrowType: String): CapabilityField = CapabilityField(CapabilityValue(path, address, borrowType))
-    fun composite(id: String, fields: Array<CompositeAttribute>): CompositeValue = CompositeValue(id, fields.toList().toTypedArray())
-    fun composite(id: String, fields: Iterable<Pair<String, Field<*>>>): CompositeValue = CompositeValue(id, fields.map { CompositeAttribute(it.first, it.second) }.toTypedArray())
-    fun composite(id: String, fields: Map<String, Field<*>>): CompositeValue = CompositeValue(id, fields.map { CompositeAttribute(it.key, it.value) }.toTypedArray())
-    fun composite(id: String, block: CDIFBuilder.() -> Array<CompositeAttribute>): CompositeValue = CompositeValue(id, block().toList().toTypedArray())
-    fun compositeOfPairs(id: String, block: CDIFBuilder.() -> Iterable<Pair<String, Field<*>>>): CompositeValue = CompositeValue(id, block().map { CompositeAttribute(it.first, it.second) }.toTypedArray())
-    fun compositeOfNamedMap(id: String, block: CDIFBuilder.() -> Map<String, Field<*>>): CompositeValue = CompositeValue(id, block().map { CompositeAttribute(it.key, it.value) }.toTypedArray())
-    fun struct(value: CompositeValue): StructField = StructField(value)
-    fun struct(block: CDIFBuilder.() -> CompositeValue): StructField = StructField(block())
-    fun resource(value: CompositeValue): ResourceField = ResourceField(value)
-    fun resource(block: CDIFBuilder.() -> CompositeValue): ResourceField = ResourceField(block())
-    fun event(value: CompositeValue): EventField = EventField(value)
-    fun event(block: CDIFBuilder.() -> CompositeValue): EventField = EventField(block())
-    fun contract(value: CompositeValue): ContractField = ContractField(value)
-    fun contract(block: CDIFBuilder.() -> CompositeValue): ContractField = ContractField(block())
-    fun <T : Enum<T>> enum(value: T): EnumField = enum(value.declaringClass.simpleName, uint8(value.ordinal))
-    fun enum(value: CompositeValue): EnumField = EnumField(value)
-    fun enum(block: CDIFBuilder.() -> CompositeValue): EnumField = EnumField(block())
-    fun enum(id: String, value: Field<*>): EnumField = EnumField(compositeOfPairs(id) { listOf("rawValue" to value) })
-    fun number(type: String, value: String): NumberField = NumberField(type, value)
-    fun number(type: String, value: Number): NumberField = NumberField(type, value.toString())
+    fun <T: Any> marshall(value: T, clazz: KClass<out T> = value::class) : Field<*> = Flow.marshall(value, clazz)
+    fun void() : VoidField = VoidField()
+    fun <T> optional(block: CDIFBuilder.() -> Field<T>?) : OptionalField<T> = OptionalField(block())
+    fun <T> optional(value: Field<T>?) : OptionalField<T> = OptionalField(value)
+    fun boolean(value: Boolean) : BooleanField = BooleanField(value)
+    fun string(value: String) : StringField = StringField(value)
+    fun array(value: Iterable<Field<*>>) : ArrayField = ArrayField(value)
+    fun <T> array(value: Iterable<T>, mapper:  CDIFBuilder.(T) -> Field<*> ) : ArrayField = ArrayField(value.map { mapper(it) })
+    fun array(block: CDIFBuilder.() -> Iterable<Field<*>>) : ArrayField = ArrayField(block().toList().toTypedArray())
+    fun dictionary(value: Iterable<DictionaryFieldEntry>) : DictionaryField = DictionaryField(value)
+    fun dictionary(block: CDIFBuilder.() -> Array<DictionaryFieldEntry>) : DictionaryField = DictionaryField(block().toList().toTypedArray())
+    fun dictionaryOfMap(block: CDIFBuilder.() -> Map<Field<*>, Field<*>>) : DictionaryField = DictionaryField(block().map { DictionaryFieldEntry(it.key, it.value) })
+    fun dictionaryOfNamedMap(block: CDIFBuilder.() -> Map<String, Field<*>>) : DictionaryField = DictionaryField(block().map { DictionaryFieldEntry(StringField(it.key), it.value) })
+    fun dictionaryOfPairs(block: CDIFBuilder.() -> Iterable<Pair<Field<*>, Field<*>>>) : DictionaryField = DictionaryField(block().map { DictionaryFieldEntry(it.first, it.second) })
+    fun dictionaryOfNamedPairs(block: CDIFBuilder.() -> Iterable<Pair<String, Field<*>>>) : DictionaryField = DictionaryField(block().map { DictionaryFieldEntry(StringField(it.first), it.second) })
+    fun address(value: String) : AddressField = AddressField(value)
+    fun address(value: ByteArray) : AddressField = AddressField(value)
+    fun path(value: PathValue) : PathField = PathField(value)
+    fun path(domain: String, identifier: String) : PathField = PathField(PathValue(domain, identifier))
+    fun capability(value: CapabilityValue) : CapabilityField = CapabilityField(value)
+    fun capability(path: String, address: String, borrowType: String) : CapabilityField = CapabilityField(CapabilityValue(path, address, borrowType))
+    fun composite(id: String, fields: Array<CompositeAttribute>) : CompositeValue = CompositeValue(id, fields.toList().toTypedArray())
+    fun composite(id: String, fields: Iterable<Pair<String, Field<*>>>) : CompositeValue = CompositeValue(id, fields.map { CompositeAttribute(it.first, it.second) }.toTypedArray())
+    fun composite(id: String, fields: Map<String, Field<*>>) : CompositeValue = CompositeValue(id, fields.map { CompositeAttribute(it.key, it.value) }.toTypedArray())
+    fun composite(id: String, block: CDIFBuilder.() -> Array<CompositeAttribute>) : CompositeValue = CompositeValue(id, block().toList().toTypedArray())
+    fun compositeOfPairs(id: String, block: CDIFBuilder.() -> Iterable<Pair<String, Field<*>>>) : CompositeValue = CompositeValue(id, block().map { CompositeAttribute(it.first, it.second) }.toTypedArray())
+    fun compositeOfNamedMap(id: String, block: CDIFBuilder.() -> Map<String, Field<*>>) : CompositeValue = CompositeValue(id, block().map { CompositeAttribute(it.key, it.value) }.toTypedArray())
+    fun struct(value: CompositeValue) : StructField = StructField(value)
+    fun struct(block: CDIFBuilder.() -> CompositeValue) : StructField = StructField(block())
+    fun resource(value: CompositeValue) : ResourceField = ResourceField(value)
+    fun resource(block: CDIFBuilder.() -> CompositeValue) : ResourceField = ResourceField(block())
+    fun event(value: CompositeValue) : EventField = EventField(value)
+    fun event(block: CDIFBuilder.() -> CompositeValue) : EventField = EventField(block())
+    fun contract(value: CompositeValue) : ContractField = ContractField(value)
+    fun contract(block: CDIFBuilder.() -> CompositeValue) : ContractField = ContractField(block())
+    fun <T : Enum<T>> enum(value: T) : EnumField = enum(value.declaringClass.simpleName, uint8(value.ordinal))
+    fun enum(value: CompositeValue) : EnumField = EnumField(value)
+    fun enum(block: CDIFBuilder.() -> CompositeValue) : EnumField = EnumField(block())
+    fun enum(id: String, value: Field<*>) : EnumField = EnumField(compositeOfPairs(id) { listOf("rawValue" to value) })
+    fun number(type: String, value: String) : NumberField = NumberField(type, value)
+    fun number(type: String, value: Number) : NumberField = NumberField(type, value.toString())
     fun int(value: Number) : IntNumberField = IntNumberField(value.toString())
     fun uint(value: Number) : UIntNumberField = UIntNumberField(value.toString())
     fun int8(value: Number) : Int8NumberField = Int8NumberField(value.toString())
@@ -147,7 +147,7 @@ class CDIFBuilder {
     fun ufix64(value: String) : UFix64NumberField = UFix64NumberField(value)
 }
 
-fun <T> parseCdif(root: Field<*>, block: CDIFParser.() -> T): T {
+fun <T> parseCdif(root: Field<*>, block: CDIFParser.() -> T) : T {
     if (root !is CompositeField) {
         throw IllegalArgumentException("root !is CompositeField")
     }
@@ -162,11 +162,11 @@ class CDIFParser {
     val compositeValue: CompositeValue get() = composite.value!!
 
     fun push(composite: CompositeField) = compositeStack.add(composite)
-    fun pop(): CompositeField = compositeStack.removeLast()
+    fun pop() : CompositeField = compositeStack.removeLast()
 
-    fun <T : Field<*>> field(name: String): T = compositeValue.getRequiredField(name)
+    fun <T : Field<*>> field(name: String) : T = compositeValue.getRequiredField(name)
 
-    fun <T> with(composite: CompositeField, block: CDIFParser.() -> T): T {
+    fun <T> with(composite: CompositeField, block: CDIFParser.() -> T) : T {
         push(composite)
         try {
             return block(this)
@@ -175,7 +175,7 @@ class CDIFParser {
         }
     }
 
-    fun <T, F : Field<*>> optional(name: String, block: CDIFParser.(field: F) -> T): T? {
+    fun <T, F : Field<*>> optional(name: String, block: CDIFParser.(field: F) -> T) : T? {
         val field = compositeValue.getField<F>(name)
         return if (field != null) {
             block(field)
@@ -184,34 +184,34 @@ class CDIFParser {
         }
     }
 
-    inline fun <reified T : Any> unmarshall(name: String): T = Flow.unmarshall(T::class, field(name))
-    inline fun <reified T : Any> unmarshall(name: String, type: KClass<T>): T = Flow.unmarshall(type, field(name))
+    inline fun <reified T : Any> unmarshall(name: String) : T = Flow.unmarshall(T::class, field(name))
+    inline fun <reified T : Any> unmarshall(name: String, type: KClass<T>) : T = Flow.unmarshall(type, field(name))
 
-    inline fun <reified T : Any> unmarshall(field: Field<*>): T = Flow.unmarshall(T::class, field)
-    inline fun <reified T : Any> unmarshall(field: Field<*>, type: KClass<T>): T = Flow.unmarshall(type, field)
+    inline fun <reified T : Any> unmarshall(field: Field<*>) : T = Flow.unmarshall(T::class, field)
+    inline fun <reified T : Any> unmarshall(field: Field<*>, type: KClass<T>) : T = Flow.unmarshall(type, field)
 
-    fun <T, F : Field<*>> field(name: String, block: CDIFParser.(field: F) -> T): T = block(field(name))
-    fun boolean(name: String): Boolean = field<BooleanField>(name).value!!
-    fun string(name: String): String = field<StringField>(name).value!!
-    fun address(name: String): String = field<AddressField>(name).value!!
-    fun short(name: String): Short = field<NumberField>(name).toShort()!!
-    fun int(name: String): Int = field<NumberField>(name).toInt()!!
-    fun long(name: String): Long = field<NumberField>(name).toLong()!!
-    fun bigInteger(name: String): BigInteger = field<NumberField>(name).toBigInteger()!!
-    fun float(name: String): Float = field<NumberField>(name).toFloat()!!
-    fun double(name: String): Double = field<NumberField>(name).toDouble()!!
-    fun bigDecimal(name: String): BigDecimal = field<NumberField>(name).toBigDecimal()!!
-    fun <T> array(name: String, block: CDIFParser.(field: ArrayField) -> T): T = block(field(name))
-    fun <T> arrayValues(name: String, mapper: CDIFParser.(field: Field<*>) -> T): List<T> = field<ArrayField>(name).value!!.map { mapper(it) }
-    fun byteArray(name: String): ByteArray = arrayValues(name) { (it as UInt8NumberField).toByte()!! }.toByteArray()
-    fun <T> dictionary(name: String, block: CDIFParser.(field: DictionaryField) -> T): T = block(field(name))
-    fun <K, V> dictionaryPairs(name: String, mapper: CDIFParser.(key: Field<*>, value: Field<*>) -> Pair<K, V>): List<Pair<K, V>> = field<DictionaryField>(name).value!!.map { mapper(it.key, it.value) }
-    fun <K, V> dictionaryMap(name: String, mapper: CDIFParser.(key: Field<*>, value: Field<*>) -> Pair<K, V>): Map<K, V> = dictionaryPairs(name, mapper).toMap()
-    inline fun <reified T : Enum<T>, V : Field<*>> enum(name: String, crossinline mapper: (V) -> T): T {
+    fun <T, F : Field<*>> field(name: String, block: CDIFParser.(field: F) -> T) : T = block(field(name))
+    fun boolean(name: String) : Boolean = field<BooleanField>(name).value!!
+    fun string(name: String) : String = field<StringField>(name).value!!
+    fun address(name: String) : String = field<AddressField>(name).value!!
+    fun short(name: String) : Short = field<NumberField>(name).toShort()!!
+    fun int(name: String) : Int = field<NumberField>(name).toInt()!!
+    fun long(name: String) : Long = field<NumberField>(name).toLong()!!
+    fun bigInteger(name: String) : BigInteger = field<NumberField>(name).toBigInteger()!!
+    fun float(name: String) : Float = field<NumberField>(name).toFloat()!!
+    fun double(name: String) : Double = field<NumberField>(name).toDouble()!!
+    fun bigDecimal(name: String) : BigDecimal = field<NumberField>(name).toBigDecimal()!!
+    fun <T> array(name: String, block: CDIFParser.(field: ArrayField) -> T) : T = block(field(name))
+    fun <T> arrayValues(name: String, mapper: CDIFParser.(field: Field<*>) -> T) : List<T> = field<ArrayField>(name).value!!.map { mapper(it) }
+    fun byteArray(name: String) : ByteArray = arrayValues(name) { (it as UInt8NumberField).toByte()!! }.toByteArray()
+    fun <T> dictionary(name: String, block: CDIFParser.(field: DictionaryField) -> T) : T = block(field(name))
+    fun <K, V> dictionaryPairs(name: String, mapper: CDIFParser.(key: Field<*>, value: Field<*>) -> Pair<K, V>) : List<Pair<K, V>> = field<DictionaryField>(name).value!!.map { mapper(it.key, it.value) }
+    fun <K, V> dictionaryMap(name: String, mapper: CDIFParser.(key: Field<*>, value: Field<*>) -> Pair<K, V>) : Map<K, V> = dictionaryPairs(name, mapper).toMap()
+    inline fun <reified T : Enum<T>, V : Field<*>> enum(name: String, crossinline mapper: (V) -> T) : T {
         return with(field(name)) {
             val field = compositeValue.getRequiredField<V>("rawValue")
             mapper(field)
         }
     }
-    inline fun <reified T : Enum<T>> enum(name: String): T = enum<T, UInt8NumberField>(name) { f -> f.toInt()!!.let { enumValues<T>()[it] } }
+    inline fun <reified T : Enum<T>> enum(name: String) : T = enum<T, UInt8NumberField>(name) { f -> f.toInt()!!.let { enumValues<T>()[it] } }
 }
