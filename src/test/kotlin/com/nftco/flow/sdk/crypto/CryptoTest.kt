c6acc14abc618a7d10fe2bf04a335e5475194c2b
lealobanov <lea.lobanov@icloud.com>
2024-01-25 13:26:29 +0800

    Working on unit tests for Crypto, KeyPair, PrivateKey, PublicKey

--- src/test/kotlin/com/nftco/flow/sdk/crypto/CryptoTest.kt
+++ src/test/kotlin/com/nftco/flow/sdk/crypto/CryptoTest.kt
@@ -1,31 +1,113 @@
 package com.nftco.flow.sdk.crypto
 
 import com.nftco.flow.sdk.HashAlgorithm
-import com.nftco.flow.sdk.SignatureAlgorithm
-import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class CryptoTest {
 
+    // KeyPair
     @Test
     fun `Can generate KeyPair`() {
-        val pair1 = Crypto.generateKeyPair(SignatureAlgorithm.ECDSA_P256)
-        val privateKey1 = Crypto.decodePrivateKey(pair1.private.hex)
-        val publicKey1 = Crypto.decodePublicKey(pair1.public.hex)
-        assertThat(pair1.private.hex).isEqualTo(privateKey1.hex)
-        assertThat(pair1.public.hex).isEqualTo(publicKey1.hex)
-
-        val pair2 = Crypto.generateKeyPair(SignatureAlgorithm.ECDSA_P256)
-        val privateKey2 = Crypto.decodePrivateKey(pair2.private.hex)
-        val publicKey2 = Crypto.decodePublicKey(pair2.public.hex)
-        assertThat(pair2.private.hex).isEqualTo(privateKey2.hex)
-        assertThat(pair2.public.hex).isEqualTo(publicKey2.hex)
+        val keyPair = Crypto.generateKeyPair()
+        assertNotNull(keyPair.private)
+        assertNotNull(keyPair.public)
     }
 
     @Test
-    fun `Can sign stuff`() {
-        val pair = Crypto.generateKeyPair()
-        val signer = Crypto.getSigner(pair.private, HashAlgorithm.SHA3_256)
-        signer.sign("testing".toByteArray())
+    fun `Test generating different key pairs`() {
+        val keyPair1 = Crypto.generateKeyPair()
+        val keyPair2 = Crypto.generateKeyPair()
+
+        assertNotEquals(keyPair1.private, keyPair2.private)
+        assertNotEquals(keyPair1.public, keyPair2.public)
     }
+
+    // PrivateKey
+    @Test
+    fun `Can decode private key`() {
+        val keyPair = Crypto.generateKeyPair()
+        val decodedPrivateKey = Crypto.decodePrivateKey(keyPair.private.hex)
+        assertNotNull(decodedPrivateKey)
+        assertEquals(keyPair.private.hex, decodedPrivateKey.hex)
+    }
+
+    @Test
+    fun `Private key throws exception when invalid`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Crypto.decodePrivateKey("invalidKey")
+        }
+    }
+
+    // PublicKey
+    @Test
+    fun `Can decode public key`() {
+        val keyPair = Crypto.generateKeyPair()
+        val decodedPublicKey = Crypto.decodePublicKey(keyPair.public.hex)
+        assertNotNull(decodedPublicKey)
+        assertEquals(keyPair.public.hex, decodedPublicKey.hex)
+    }
+
+    @Test
+    fun `Public key throws exception when invalid`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Crypto.decodePublicKey("invalidKey")
+        }
+    }
+
+    // Crypto
+    @Test
+    fun `Get signer`() {
+        val keyPair = Crypto.generateKeyPair()
+        val signer = Crypto.getSigner(keyPair.private)
+        assertNotNull(signer)
+    }
+
+    @Test
+    fun `Get hasher`() {
+        val hasher = Crypto.getHasher()
+        assertNotNull(hasher)
+    }
+
+    @Test
+    fun `Normalize signature`() {
+        val keyPair = Crypto.generateKeyPair()
+        val data = "test".toByteArray()
+
+        val originalSignature = Crypto.getSigner(keyPair.private).sign(data)
+        val normalizedSignature = Crypto.normalizeSignature(originalSignature, keyPair.private.ecCoupleComponentSize)
+
+        assertEquals(2 * keyPair.private.ecCoupleComponentSize, normalizedSignature.size)
+    }
+
+    @Test
+    fun `Extract RS from valid signature`() {
+        val keyPair = Crypto.generateKeyPair()
+        val signer = SignerImpl(keyPair.private, HashAlgorithm.SHA3_256)
+        val signature = signer.sign("test".toByteArray())
+
+        val (r, s) = Crypto.extractRS(signature)
+        println(r)
+        println(s)
+
+        // Assert
+    }
+
+    @Test
+    fun `Hasher implementation`() {
+        val hasher = HasherImpl(HashAlgorithm.SHA3_256)
+        val hashedBytes = hasher.hash("test".toByteArray())
+        assertNotNull(hashedBytes)
+    }
+
+    @Test
+    fun `Signer implementation`() {
+        val keyPair = Crypto.generateKeyPair()
+        val signer = SignerImpl(keyPair.private, HashAlgorithm.SHA3_256)
+        val signature = signer.sign("test".toByteArray())
+        assertNotNull(signature)
+    }
+
+    // test exception handling on sign()
+
 }
