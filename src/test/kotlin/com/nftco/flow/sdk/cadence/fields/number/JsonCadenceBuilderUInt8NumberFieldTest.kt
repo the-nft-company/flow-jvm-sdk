6128f98fddb8e876d26a75e1e71667feecb5d7c8
Lea Lobanov <lea.lobanov@icloud.com>
2024-02-03 02:12:49 +0800

    Working on tests for number fields

---
+++ src/test/kotlin/com/nftco/flow/sdk/cadence/fields/number/JsonCadenceBuilderUInt8NumberFieldTest.kt
@@ -0,0 +1,27 @@
+package com.nftco.flow.sdk.cadence.fields.number
+
+import com.nftco.flow.sdk.cadence.UInt8NumberField
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class JsonCadenceBuilderUInt8NumberFieldTest {
+    @Test
+    fun `Test creating UInt8NumberField with valid value`() {
+        val uint8Field = UInt8NumberField("42")
+        assertEquals("42", uint8Field.value)
+    }
+
+    @Test
+    fun `Test hashCode on UInt8NumberField`() {
+        val uint8Field1 = UInt8NumberField("123")
+        val uint8Field2 = UInt8NumberField("123")
+        assertEquals(uint8Field1.hashCode(), uint8Field2.hashCode())
+    }
+
+    @Test
+    fun `Test creating UInt8NumberField with invalid value`() {
+        Assertions.assertThatThrownBy { UInt8NumberField("invalidValue").decodeToAny() }
+            .isInstanceOf(NumberFormatException::class.java)
+    }
+}
